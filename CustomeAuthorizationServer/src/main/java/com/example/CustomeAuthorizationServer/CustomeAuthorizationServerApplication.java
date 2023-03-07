ff277656e24c1a2b2a85e46d8e0189209bf575d6
LAPTOP-SRFJQ8HC\10raj <10rajprajapati@gmail.com>
2023-03-08 00:54:33 +0530

    08032023 Implemented cutome custome authorization server

---
+++ CustomeAuthorizationServer/src/main/java/com/example/CustomeAuthorizationServer/CustomeAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CustomeAuthorizationServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomeAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomeAuthorizationServerApplication.class, args);
+	}
+
+}
